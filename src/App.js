f0f5c181fb36fa93a213fcc1217bd779d04470ec
FridahOmari <nginaomari@gmail.com>
2026-01-03 16:16:18 +0300

    Initial project commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameChange = () => {
+    const names = ['Fifi', 'Soni','Kendi'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+  const name = handleNameChange();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {name}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
