7bb5193183691b3a92b183d852e9726a8e8937ab
FridahOmari <nginaomari@gmail.com>
2026-01-04 04:25:01 +0300

    added a header, content and footer files to the project.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-  const handleNameChange = () => {
-    const names = ['Fifi', 'Soni','Kendi'];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  }
-  const name = handleNameChange();
+ 
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {name}!
-        </p>
-      </header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
